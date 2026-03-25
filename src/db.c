8cb0030021475e0b247a2a12b86508d7cdadf669
Markel Trigo Cortazar <markel.trigo@opendeusto.es>
2026-03-25 11:02:06 +0100

    Creada la base de datos para la primera entrega y los archivos donde hacer sus peticiones.

---
+++ src/db.c
@@ -0,0 +1,14 @@
+/*
+ * db.c
+ *
+ *  Created on: 25 mar 2026
+ *      Author: markel.trigo
+ */
+
+#include <stdio.h>
+#include "sqlite3.h"
+#include <string.h>
+#include  "db.h"
+
+
+
---
+++ src/db.h
@@ -0,0 +1,13 @@
+/*
+ * db.h
+ *
+ *  Created on: 25 mar 2026
+ *      Author: markel.trigo
+ */
+
+#ifndef DB_H_
+#define DB_H_
+
+
+
+#endif /* DB_H_ */
