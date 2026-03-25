f1b5ee1b60d2cf6a5daedd8e2b0518e006fe7e7c
Markel Trigo Cortazar <markel.trigo@opendeusto.es>
2026-03-25 11:23:31 +0100

    Creado el registro para logs

--- src/db.c
+++ src/db.c
@@ -11,4 +11,20 @@
 #include  "db.h"
 
 
+int registrar_log(sqlite3 *db, int id_usuario, const char *nivel, const char *mensaje) {
+    sqlite3_stmt *stmt;
+    const char *sql = "INSERT INTO log (id_usuario, nivel, mensaje, fecha) VALUES (?, ?, ?, datetime('now'));";
 
+    if (sqlite3_prepare_v2(db, sql, -1, &stmt, NULL) != SQLITE_OK) {
+        return sqlite3_errcode(db);
+    }
+
+    sqlite3_bind_int(stmt, 1, id_usuario);
+    sqlite3_bind_text(stmt, 2, nivel, -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 3, mensaje, -1, SQLITE_STATIC);
+
+    int rc = sqlite3_step(stmt);
+
+    sqlite3_finalize(stmt);
+    return (rc == SQLITE_DONE) ? SQLITE_OK : rc;
+}
--- src/db.h
+++ src/db.h
@@ -8,6 +8,6 @@
 #ifndef DB_H_
 #define DB_H_
 
-
+int registrar_log(sqlite3 *db, int id_usuario, const char *nivel, const char *mensaje);
 
 #endif /* DB_H_ */
