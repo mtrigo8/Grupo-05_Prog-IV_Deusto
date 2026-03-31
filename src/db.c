6cffcde1be1c8e0c3d7a58bc88d21060957258f8
Markel Trigo Cortazar <markel.trigo@opendeusto.es>
2026-03-31 10:18:24 +0200

    Falta por acabar la funcion insertar usuario

--- src/db.c
+++ src/db.c
@@ -28,3 +28,75 @@ int registrar_log(sqlite3 *db, int id_usuario, const char *nivel, const char *me
     sqlite3_finalize(stmt);
     return (rc == SQLITE_DONE) ? SQLITE_OK : rc;
 }
+
+int borrar_reservas(sqlite3 *db) {
+	sqlite3_stmt *stmt;
+
+	char sql[] = "delete from reserva";
+
+	int result = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL) ;
+	if (result != SQLITE_OK) {
+		printf("Error preparing statement (DELETE)\n");
+		printf("%s\n", sqlite3_errmsg(db));
+		return result;
+	}
+
+	printf("SQL query prepared (DELETE)\n");
+
+	result = sqlite3_step(stmt);
+	if (result != SQLITE_DONE) {
+		printf("Error deleting data\n");
+		printf("%s\n", sqlite3_errmsg(db));
+		return result;
+	}
+
+	result = sqlite3_finalize(stmt);
+	if (result != SQLITE_OK) {
+		printf("Error finalizing statement (DELETE)\n");
+		printf("%s\n", sqlite3_errmsg(db));
+		return result;
+	}
+
+	printf("Prepared statement finalized (DELETE)\n");
+
+	return SQLITE_OK;
+}
+
+//Falta acabarlo
+int insert_usuario(sqlite3 *db, char datos[][]) {
+	sqlite3_stmt *stmt;
+
+	char sql[] = "insert into country (id_usuario, nombre, DNI, password_hash, id_rol) values (NULL, ?, ?, ?, 2)";
+	int result = sqlite3_prepare_v2(db, sql, strlen(sql) + 1, &stmt, NULL) ;
+	if (result != SQLITE_OK) {
+		printf("Error preparing statement (INSERT)\n");
+		printf("%s\n", sqlite3_errmsg(db));
+		return result;
+	}
+
+	printf("SQL query prepared (INSERT)\n");
+
+	result = sqlite3_bind_text(stmt, 1, name, strlen(name), SQLITE_STATIC);
+	if (result != SQLITE_OK) {
+		printf("Error binding parameters\n");
+		printf("%s\n", sqlite3_errmsg(db));
+		return result;
+	}
+
+	result = sqlite3_step(stmt);
+	if (result != SQLITE_DONE) {
+		printf("Error inserting new data into country table\n");
+		return result;
+	}
+
+	result = sqlite3_finalize(stmt);
+	if (result != SQLITE_OK) {
+		printf("Error finalizing statement (INSERT)\n");
+		printf("%s\n", sqlite3_errmsg(db));
+		return result;
+	}
+
+	printf("Prepared statement finalized (INSERT)\n");
+
+	return SQLITE_OK;
+}
--- src/db.h
+++ src/db.h
@@ -9,5 +9,8 @@
 #define DB_H_
 
 int registrar_log(sqlite3 *db, int id_usuario, const char *nivel, const char *mensaje);
+int borrar_reservas(sqlite3 *db);
+int insert_usuario(sqlite3 *db, char datos[][]);
+
 
 #endif /* DB_H_ */
